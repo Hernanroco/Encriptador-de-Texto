fbedf6fc5e5b6e896ffe85aa63132c94f856f2a9
Hernan Rodriguez <hernan.roco@yahoo.com>
2024-01-28 12:42:35 -0500

    modificaciones

--- index.js
+++ index.js
@@ -2,33 +2,14 @@ function encriptar () {
   let tituloMensaje = document.getElementById('titulo-mensaje');
   let parrafo = document.getElementById('parrafo');
   let texto = document.getElementById('texto').value;
-  let textoCifrado = texto.replace(/a/gi, 'd')
-                          .replace(/b/gi, 'c')
-                          .replace(/c/gi, 'z')
-                          .replace(/d/gi, 'i')
-                          .replace(/e/gi, 'f')
-                          .replace(/f/gi, 'a')
-                          .replace(/g/gi, 'f')
-                          .replace(/h/gi, 'o')
-                          .replace(/i/gi, 'u')
-                          .replace(/j/gi, 'm')
-                          .replace(/k/gi, 'l')
-                          .replace(/l/gi, 'x')
-                          .replace(/m/gi, 'y')
-                          .replace(/n/gi, 'v')
-                          .replace(/ñ/gi, 'q')
-                          .replace(/o/gi, 'e')
-                          .replace(/p/gi, 'j')
-                          .replace(/q/gi, 'p')
-                          .replace(/r/gi, 'r')
-                          .replace(/s/gi, 'z')
-                          .replace(/t/gi, 'w')
-                          .replace(/u/gi, 'ñ')
-                          .replace(/v/gi, 'n')
-                          .replace(/w/gi, 'b')
-                          .replace(/x/gi, 'h')
-                          .replace(/y/gi, 'i')
-                          .replace(/z/gi, 'k');
+  let textoCifrado = texto.replace(/a/gi, 'zu')
+                          .replace(/e/gi, 'zi')
+                          .replace(/i/gi, 'za')
+                          .replace(/o/gi, 'zo')
+                          .replace(/u/gi, 'ze')
+                          .replace(/h/gi, 're')
+                          .replace(/l/gi, 'ro')
+                          
 
   if(texto.length != 0) {
     document.getElementById('texto').value = textoCifrado;
@@ -42,5 +23,25 @@ function encriptar () {
 }
 
 function desencriptar () {
-  
+  let texto = document.getElementById('texto').value;
+  let textoDescifrado = texto.replace(/zu/gi, 'a')
+                             .replace(/zi/gi, 'e')
+                             .replace(/za/gi, 'i')
+                             .replace(/zo/gi, 'o')
+                             .replace(/ze/gi, 'u')
+                             .replace(/re/gi, 'h')
+                             .replace(/ro/gi, 'l')
+                          
+
+  if(texto.length != 0) {
+    document.getElementById('texto').value = textoDescifrado;
+    tituloMensaje.textContent = 'Texto desencriptado con exito!';
+    parrafo.textContent = '';
+  } else {
+    tituloMensaje.textContent = 'Ningun mensaje encontrado';
+    parrafo.textContent = 'Ingresa el texto que deseea encriptar o desencriptar';
+    alert('Debes ingresar algun texto');
+  }
+
 }
+
